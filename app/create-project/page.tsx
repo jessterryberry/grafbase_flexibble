2d8a0b54355885e0e67cf48807db2687cdbb2c17
Jessica Terryberry <jterryberry@niagaracollege.ca>
2023-08-03 13:07:32 -0400

    modal functionality

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -1,6 +1,19 @@
-const CreateProject = () => {
+import Modal from "@/components/Modal"
+import ProjectForm from "@/components/ProjectForm"
+import { getCurrentUser } from "@/lib/session"
+import { redirect } from "next/navigation";
+
+const CreateProject = async () => {
+    const session = await getCurrentUser();
+
+    if(!session?.user) redirect('/');
+
   return (
-    <div>CreateProject</div>
+    <Modal>
+        <h3 className="modal-head-text">Create a New Project</h3>
+
+        <ProjectForm type="create" session={session}/>
+    </Modal>
   )
 }
 
