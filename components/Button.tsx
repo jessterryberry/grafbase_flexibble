c463de5232133c5b800d14601fa291e472b1e236
Jessica Terryberry <jterryberry@niagaracollege.ca>
2023-08-10 12:38:04 -0400

    project upload additions

--- components/Button.tsx
+++ components/Button.tsx
@@ -18,8 +18,8 @@ const Button = ({ title, leftIcon, rightIcon, handleClick, isSubmitting, type, b
         type={type || 'button'}
         disabled={isSubmitting}
         className={`flexCenter gap-3 px-4 py-3
-            ${textColor ? textColor : 'text-white'}
-            ${isSubmitting ? 'bg-black/50' : bgColor ? bgColor : 'bg-primary-purple'} rounded-xl text-sm font-medium max-md:w-full
+            ${textColor || 'text-white'}
+            ${isSubmitting ? 'bg-black/50' : bgColor || 'bg-primary-purple'} rounded-xl text-sm font-medium max-md:w-full
         `}
         onClick={handleClick}
     >
--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,12 +1,14 @@
 "use client"
 
-import { SessionInterface } from "@/common.types"
-import { ChangeEvent, useState } from "react"
-import Image from "next/image"
-import FormField from "./FormField"
-import { categoryFilters } from "@/constants"
-import CustomMenu from "./CustomMenu"
-import Button from "./Button"
+import { SessionInterface } from "@/common.types";
+import { ChangeEvent, useState } from "react";
+import Image from "next/image";
+import FormField from "./FormField";
+import { categoryFilters } from "@/constants";
+import CustomMenu from "./CustomMenu";
+import Button from "./Button";
+import { createNewProject, fetchToken } from "@/lib/actions";
+import { useRouter } from "next/navigation";
 
 type Props = {
     type: string,
@@ -14,8 +16,27 @@ type Props = {
 }
 
 const ProjectForm = ({ type, session }: Props) => {
+  const router = useRouter();
 
-  const handleFormSubmit = (e: React.FormEvent) => {};
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setisSubmitting(true);
+
+    const token = await fetchToken();
+
+    try {
+      if(type === 'create'){
+        await createNewProject(form, session?.user?.id, token);
+
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setisSubmitting(false);
+    }
+  };
 
   const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
