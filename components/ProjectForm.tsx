24d3b18ed60711b9876f0bd148b2c251164d706d
Jessica Terryberry <jterryberry@niagaracollege.ca>
2023-08-17 11:11:36 -0400

    projectform nullable

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -13,7 +13,7 @@ import { useRouter } from "next/navigation";
 type Props = {
     type: string,
     session: SessionInterface,
-    project: ProjectInterface
+    project?: ProjectInterface
 }
 
 const ProjectForm = ({ type, session, project }: Props) => {
